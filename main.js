8189fa65b33fa3f3dd186421099e579d907cf5d5
Poltree <76885489+Poltree@users.noreply.github.com>
2021-04-17 13:52:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -6,18 +6,15 @@ background_image="race_track.png";
 car1_image="car1.png";
 car1_x=10;
 car1_y=10;
-
 car2_height=70;
 car2_width=120;
 car2_image="car2.png";
 car2_x=10;
 car2_y=100;
-
 function add(){
     background_imgTag = new Image();
     background_imgTag.onload = uploadBackground;
     background_imgTag.src = background_image;
-
     car1_imgTag = new Image();
     car1_imgTag.onload = uploadcar1;
     car1_imgTag.src = car1_image;
@@ -26,11 +23,9 @@ function add(){
     car2_imgTag.onload = uploadcar2;
     car2_imgTag.src = car2_image;
 };
-
 function uploadBackground(){
     ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 }
-
 function uploadcar1(){
     ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
 }
@@ -38,7 +33,6 @@ function uploadcar2(){
     ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
 }
 window.addEventListener("keydown", my_keydown)
-
 function my_keydown(e){
     keyPressed = e.keyCode;
     console.log(keyPressed);
